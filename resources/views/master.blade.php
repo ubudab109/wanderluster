de0c64ae7aa9a7c0f4f7dbf21af4788ca75366fc
Muhammad Rizky Firdaus <167732712+dausrizky109@users.noreply.github.com>
2024-06-12 21:12:55 +0700

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wenderluster.id</title>
+
+    <!-- font awesome cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+    <!-- font google nya adalah Philosopher -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md navbar-light fixed-top">
+
+        <a class="navbar-brand kotak-logo">
+            <img src="{{asset('assets/img/Logo Putih.svg')}}" class="white_logo">
+            <img src="{{asset('assets/img/orange_logo.svg')}}" class="orange_logo">
+        </a>
+
+        <button type="button" class="navbar-toggler bg-light" data-toggle="collapse" data-target="#nav">
+
+            <span class="navbar-toggler-icon"></span>
+
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-center" id="nav">
+            <ul class="navbar-nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link text-light font-weight-bold px-3" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light font-weight-bold px-3" href="#">Our Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light font-weight-bold px-3" href="#">Destination</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light font-weight-bold px-3" href="#">Tour & Travel</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light font-weight-bold px-3 contact_nav" href="#">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+        <span class="text-light font-weight-bold px-3 right-side-nav">
+            Contact Us
+        </span>
+
+    </nav>
+    @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,61 @@
+@extends('master')
+@section('content')
+<!-- Awal Header -->
+{{-- <header>
+    <input type="checkbox" name="" id="toggler">
+    <label for="toggler" class="fas-fa-bars"></label>
+    <div class="kotak-logo">
+        <a href="#" class="logo"><img src="{{asset('assets/img/Logo Putih.svg')}}" alt=""></a>
+    </div>
+
+    <nav class="navbar">
+        <a href="#home">Home</a>
+        <a href="#service">Our Services</a>
+        <a href="#destination">Destination</a>
+        <a href="#service">Tour & Travel</a>
+    </nav>
+    <div class="contact">
+        <a href="#contact"> Contact Us</a>
+    </div>
+
+
+    <!-- Akhir Header -->
+</header> --}}
+
+<!--  Awal Home Section -->
+<section class="home" id="#home">
+    <div class="content">
+        <h3>Bali-Lombok</h3>
+        <p>3 Days 2 Night</p>
+        <a href="#" class="btn"> Book Now</a>
+    </div>
+</section>
+<!-- Akhir Home Section -->
+<section>
+    <div class="row mt-3">
+        <div class="col-xl-6 col-lg-12">
+            <h1 class="heading">About Us</h1>
+            <h2 class="moto mb-3">We bring you the world</h2>
+            <p style="font-size: 18px;">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum error dicta nihil veritatis iusto,
+                similique
+                pariatur, sint fuga asperiores eaque blanditiis possimus culpa quis soluta? Beatae, saepe tempore
+                voluptates
+                labore ipsam molestias reiciendis adipisci quaerat perferendis, vero voluptatibus quo nesciunt corrupti
+                obcaecati laboriosam ratione officia, quibusdam temporibus doloribus totam aperiam!
+            </p>
+        </div>
+    </div>
+</section>
+{{-- <div class="about" id="about">
+
+    <h1 class="heading">About Us</h1>
+    <h2 class="moto">We bring you the world</h2>
+
+
+</div> --}}
+<!-- About Section Start -->
+
+
+<!-- About Section End -->
+@endsection
